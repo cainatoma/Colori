8884667a6c253c1e36eeb2743302e43f3867cffc
cainatoma <cainatomaofficial@gmail.com>
2025-11-21 11:51:15 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1377810898"
+    serviceWorkerVersion: "4079967046"
   }
 });
